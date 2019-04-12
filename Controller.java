91c65f590af585a526633aeab05bba84d1dad79d
docong123 <48306520+docong123@users.noreply.github.com>
2019-04-12 22:51:55 +0700

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,28 @@
+package sample;
+
+public abstract class Controller {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Controller(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public abstract void toStringg();
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
-package Lab_Session6;
+package sample;
 
 public class Main {
     public static void main(String[] args) {
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.insertPhone();
+        PersonModel<student> studentPersonModel = new PersonModel<>();
+        studentPersonModel.add(new student(1, 18, " Kiên"));
+        studentPersonModel.add(new student(2, 19, " Thông"));
+        studentPersonModel.input();
+        PersonModel<Employee> employeePersonModel = new PersonModel<>();
+        employeePersonModel.add(new Employee(3, 20.0000, " Đít"));
+        employeePersonModel.add(new Employee(4, 30.000, " Giang"));
+        employeePersonModel.input();
     }
 }
